c5dae0d3f34a5c840ab27eebe32b447b6f2faf47
Niza Toshpulatov <niza@hey.com>
2024-05-17 14:26:04 +0200

    Tidy up

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
-require("rpm_init").setup({
-  after_init = function()
-    require("lsp")
-    require("keymaps")
-  end
-})
+-- Initialize RPM (a plugin manager) and load plugins
+require("rpm_init").setup()
 
+-- Use Oil (a file explorer) instead of netrw
 require("oil").setup()
 
+-- Load the rest of the configuration
+require("lsp")
 require("colorscheme")
 require("options")
+require("keymaps")
 require("statusline")
 require("user_commands")
